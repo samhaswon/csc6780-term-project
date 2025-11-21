73a4d84c1b11a946ca00277aba684ebb32e0f5af
samhaswon <sahoward42@tntech.edu>
2025-11-21 15:26:29 -0600

    Fix model download script paths

--- download_models.sh
+++ download_models.sh
@@ -5,8 +5,8 @@ wget https://github.com/samhaswon/csc6780-term-project/releases/download/v0.0.0/
 wget https://github.com/samhaswon/csc6780-term-project/releases/download/v0.0.0/birefnet.pth -O models/birefnet.pth
 wget https://github.com/samhaswon/csc6780-term-project/releases/download/v0.0.0/u2net.onnx -O models/u2net.onnx
 wget https://github.com/samhaswon/csc6780-term-project/releases/download/v0.0.0/u2net.pth -O models/u2net.pth
-wget https://github.com/samhaswon/csc6780-term-project/releases/download/v0.0.0/u2netp.onnx -O u2netp.onnx
-wget https://github.com/samhaswon/csc6780-term-project/releases/download/v0.0.0/u2netp.pth -O u2netp.pth
-wget https://github.com/samhaswon/csc6780-term-project/releases/download/v0.0.0/u2netp_chunks.onnx -O u2netp_chunks.onnx
-wget https://github.com/samhaswon/csc6780-term-project/releases/download/v0.0.0/u2netp_chunks.pth -O u2netp_chunks.pth
+wget https://github.com/samhaswon/csc6780-term-project/releases/download/v0.0.0/u2netp.onnx -O models/u2netp.onnx
+wget https://github.com/samhaswon/csc6780-term-project/releases/download/v0.0.0/u2netp.pth -O models/u2netp.pth
+wget https://github.com/samhaswon/csc6780-term-project/releases/download/v0.0.0/u2netp_chunks.onnx -O models/u2netp_chunks.onnx
+wget https://github.com/samhaswon/csc6780-term-project/releases/download/v0.0.0/u2netp_chunks.pth -O models/u2netp_chunks.pth
 echo "Download complete"
