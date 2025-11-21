b26eeafb77b8b57d82ba99d75f2c751ed85da6de
samhaswon <sahoward42@tntech.edu>
2025-11-21 15:22:50 -0600

    Add model download script

---
+++ download_models.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "Downloading Models to ./models/"
+wget https://github.com/samhaswon/csc6780-term-project/releases/download/v0.0.0/birefnet.onnx -O models/birefnet.onnx
+wget https://github.com/samhaswon/csc6780-term-project/releases/download/v0.0.0/birefnet.pth -O models/birefnet.pth
+wget https://github.com/samhaswon/csc6780-term-project/releases/download/v0.0.0/u2net.onnx -O models/u2net.onnx
+wget https://github.com/samhaswon/csc6780-term-project/releases/download/v0.0.0/u2net.pth -O models/u2net.pth
+wget https://github.com/samhaswon/csc6780-term-project/releases/download/v0.0.0/u2netp.onnx -O u2netp.onnx
+wget https://github.com/samhaswon/csc6780-term-project/releases/download/v0.0.0/u2netp.pth -O u2netp.pth
+wget https://github.com/samhaswon/csc6780-term-project/releases/download/v0.0.0/u2netp_chunks.onnx -O u2netp_chunks.onnx
+wget https://github.com/samhaswon/csc6780-term-project/releases/download/v0.0.0/u2netp_chunks.pth -O u2netp_chunks.pth
+echo "Download complete"
